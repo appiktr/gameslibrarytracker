2121502753b349978162c0c777865c343456831e
appik <appik@unknown72579d6aadec.home>
2026-01-20 10:59:35 +0300

    yt

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,6 @@
-import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   static String get steamApiKey => dotenv.env['STEAM_API_KEY'] ?? '';
-  static String get baseUrl {
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8080';
-    }
-    return 'http://localhost:8080';
-  }
+  static String get baseUrl => 'http://igsgk04sow0g4wwccwc40g4g.46.224.57.253.sslip.io';
 }
